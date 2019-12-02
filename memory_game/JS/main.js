6798acdf0a69caa5641f2335a2ac184b60de4e52
proust01 <proust0319@gmail.com>
2019-12-02 11:51:31 +0800

    created function

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,21 +1,25 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+function checkForMatch() {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    }else{
+        console.log("Sorry, try again");
+    }
 
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-
-if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-}else{
-    alert("Sorry, try again");
 }
+
+function flipCard(cardId) {
+    
+    console.log("User flipped " + cards[cardId]);
+    
+    cardsInPlay.push(cards[cardId]);
+
+    checkForMatch();
+
+}
+
+flipCard(0);
+flipCard(2);
